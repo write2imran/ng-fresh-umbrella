9152859a93f2db13de530b3c8e2351d1ef81d27f
Imran Rauf (Dot) <imranax@outlook.com>
2023-06-26 10:35:06 +0300

    primeng setup

--- primeng/README.md
+++ primeng/README.md
@@ -1,4 +1,4 @@
-# Material
+# Primeng
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.1.
 
--- primeng/src/app/app.module.ts
+++ primeng/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
